566612ce305285f15a6705c46875c3a6f35ccabe
official-mafuzalam <everythinggood885@gmail.com>
2025-09-13 09:54:04 +0600

    first commit

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Create roles and permissions
+        $roles = [
+            'super_admin',
+            'admin',
+            'user'
+        ];
+        foreach ($roles as $role) {
+            Role::create(['name' => $role]);
+        }
+
+        $permissions = [
+            'super_admin',
+            'special',
+            'read',
+            'write',
+            'edit',
+            'delete',
+            'block_user',
+            'user_assign_role',
+            'user_assign_permission',
+            'user_edit',
+            'user_delete'
+        ];
+        foreach ($permissions as $permission) {
+            Permission::create(['name' => $permission]);
+        }
+
+        // Assign all permissions to super_admin
+        $superAdminRole = Role::where('name', 'super_admin')->first();
+        if ($superAdminRole) {
+            $superAdminRole->syncPermissions($permissions);
+        }
+    }
+}
